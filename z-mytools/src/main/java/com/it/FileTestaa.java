77aa70a86caebc5210af52eea6f12f97925cdfb6
xm <616326125@qq.com>
2021-07-02 17:12:05 +0800

    nginx.md

--- z-mytools/src/main/java/com/it/FileTestaa.java
+++ z-mytools/src/main/java/com/it/FileTestaa.java
@@ -1,17 +1,33 @@
 package com.it;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 
 import java.io.File;
 import java.util.*;
 
 public class FileTestaa {
     public static void main(String[] args) {
-        HashMap<String, Object> map = new HashMap<>();
-        map.put("status",100);
-        System.out.println((int) map.get("status"));
-        System.out.println((String) map.get("status"));
-        System.out.println( map.get("status")instanceof Integer);
-        System.out.println( map.get("status") instanceof  String);
+        Map<String, Object> pushMap = new HashMap<>(2);
+        pushMap.put("loginName", "loginName");
+        Map<String, String> informationMap = new HashMap<>(8);
+        informationMap.put("applyId","applyId" );
+        informationMap.put("companyName","companyName" );
+        informationMap.put("creditCode", "creditCode");
+        informationMap.put("legalRepresentative", "legalRepresentative");
+        informationMap.put("contact", "contact");
+        informationMap.put("contactMobile", "contactMobile");
+        informationMap.put("contactFax","contactFax" );
+        informationMap.put("postalCode","postalCode" );
+        informationMap.put("address", "address");
+        informationMap.put("province", "province");
+        informationMap.put("city", "city");
+        informationMap.put("county", "county");
+        informationMap.put("remark", "remark");
+        informationMap.put("fileNos","fileNos" );
+        pushMap.put("informationAddData", informationMap);
+
+        String jsonString = JSONObject.toJSONString(pushMap);
+        System.out.println(jsonString);
     }
 }
